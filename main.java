4f1bae4eebd1cb0072e36b3974f3be784ac2d13e
Guilherme Silva <ajenteduplo000@gmail.com>
2026-09-23 19:48:39 -0300

    Melhoria da logica base
    Refactor calculator program to handle two numbers and operations.

--- main.java
+++ main.java
@@ -1,15 +1,21 @@
-
 import java.util.Scanner;
+import java.math.BigDecimal;
 
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner scanne = new Scanner(System.in);
-	    System.out.println("===Calculadora Simples===");
-		System.out.print("Digite seu numero:");
-		Double num = scanne.nextDouble();
-		System.out.println("Escolha entre (+ / - / * / %): ");
-		char op = scanne.next().charAt(0);
-		System.out.printf("Resultado: %.2f %c", num, op);
-	}
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um numeor: ");
+        double num = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Escolha entre (* / % / + / -):");
+        String op = scanner.nextLine();
+        System.out.print("Escolha outro numeor: ");
+        double num2 = scanner.nextDouble();
+
+        switch (op) {
+                case "+": System.out.printf("%.f2 %c %.f2 = %.f2", num, op, num2, num2);
+        }
+        //System.out.printf("Numero digitado foi: %.2f", num);
+    }
 }
